691aeb144880435d79163b93bd332d594eb0979c
Louis-Philippe David <9373382@bdeb.qc.ca>
2025-10-22 17:05:27 -0400

    Fix SSL connection parameter for production database

--- Hobart/settings/production.py
+++ Hobart/settings/production.py
@@ -13,5 +13,5 @@ ALLOWED_HOSTS = allowed_hosts_env.split(',') if allowed_hosts_env else []
 
 # Production database configuration
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(conn_max_age=600)
 }
