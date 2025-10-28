e37a76cf259e8b73229db818b7b9f81670ec04db
Louis-Philippe David <9373382@bdeb.qc.ca>
2025-10-28 14:26:03 -0400

    fix: Configure ALLOWED_HOSTS for Cloud Run

--- Hobart/settings/postgres.py
+++ Hobart/settings/postgres.py
@@ -2,14 +2,19 @@ from .base import *
 import dj_database_url
 import os
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+# Check if we are running in a Google Cloud Run environment
+if 'K_SERVICE' in os.environ:
+    # Production settings on Cloud Run
+    DEBUG = False
+    # Trust the automatically provided hostname
+    ALLOWED_HOSTS = [os.environ.get('K_SERVICE') + '.run.app', '.run.app']
+else:
+    # Local development settings
+    DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-postgres-development-key')
-
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
